9b47f8f55d1a06364c7cfae1e85b34a36d211e93
dudagoisclaret08 <dudagois86@gmail.com>
2025-06-24 15:30:03 -0300

    Add files via upload

---
+++ madu.py
@@ -0,0 +1,24 @@
+import pyautogui 
+
+
+
+#pyautogui.mouseInfo()google chrome
+pyautogui.press("win")
+pyautogui.sleep(2)
+pyautogui.write("google chrome" )
+pyautogui.sleep(1)
+pyautogui.press("enter") 
+pyautogui.sleep(1)
+pyautogui.write("instagram")
+pyautogui.sleep(1)
+pyautogui.press("enter")
+pyautogui.sleep(1)
+pyautogui.click(277,296)
+pyautogui.sleep(1)
+pyautogui.click(550,388)
+pyautogui.write("ascdcg")
+pyautogui.sleep(1)
+pyautogui.click(502,433)
+pyautogui.write("morango")
+pyautogui.press("enter")
+
